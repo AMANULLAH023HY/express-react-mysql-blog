52f98988379e056da9288048f985586ed0b87a36
AMANULLAH023HY <amana170829@gmail.com>
2024-04-20 16:48:03 +0530

    Register and Login UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider, Route, Outlet } from 'react-router-dom';
+import './style.scss';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Single from './pages/Single';
+import Write from './pages/Write';
+import NavBar from './component/NavBar';
+import Footer from './component/Footer';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+const Layout = ()=>{
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar/>
+      <Outlet/>
+      <Footer/>
     </>
   )
 }
 
-export default App
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<Layout/>,
+    children:[
+      {
+        path: '/',
+        element: <Home/>
+      },
+    
+      {
+        path: '/post/:id',
+        element: <Single/>
+      },
+    
+      {
+        path: '/write',
+        element: <Write/>
+      },
+
+    ]
+      
+  },
+  {
+    path: '/register',
+    element: <Register/>
+  },
+  
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/single',
+    element: <Single/>
+  },
+
+  {
+    path: '/write',
+    element: <Write/>
+  },
+  
+  
+  
+  
+]);
+
+function App() {
+  return (
+    <div className='app'>
+    <div className='container'>
+
+      <RouterProvider router={router}/>
+    </div>
+        
+    </div>
+  );
+}
+
+
+
+export default App;
